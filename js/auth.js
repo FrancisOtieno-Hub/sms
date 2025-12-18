4273804c261cde74e2ad010a6ea7b121b7518510
Francis Otieno <info.francisotieno@gmail.com>
2025-12-18 07:34:21 +0300

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,17 @@
+import { supabase } from "./supabase.js";
+
+window.login = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    window.location.href = "dashboard.html";
+  }
+};
