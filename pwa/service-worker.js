cd3bba8e3d7bf7013358ce9a6632879ed6b02e65
Francis Otieno <info.francisotieno@gmail.com>
2025-12-18 07:38:38 +0300

    Create service-worker.js

---
+++ pwa/service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("sms-cache").then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/css/style.css"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
